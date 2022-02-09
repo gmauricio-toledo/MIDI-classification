ad80bc44d6380cd585cc24190505d695783645c0
gmauricio-toledo <mauricio.toledo@uaem.mx>
2022-02-08 21:21:20 -0600

    starting

---
+++ Resources/midivis-master/README.md
@@ -0,0 +1,6 @@
+# Visualising MIDI with Python and Matplotlib
+A fun little project to turn MIDI files into charts with `python-midi` and `matplotlib`.
+
+To run the script:
+- `pip install -r requirements.txt` (recommended to do this in a virtualenv)
+- `python midivis.py [PATH TO MIDI FILE TO PLOT]`
---
+++ Resources/midivis-master/midivis.py
@@ -0,0 +1,19 @@
+import midi
+import matplotlib.pyplot as plt
+import argparse
+
+parser = argparse.ArgumentParser(description="Turn a .midi file into a graph")
+parser.add_argument('midi_file', help='Path to .midi file to process')
+
+args = parser.parse_args()
+
+song = midi.read_midifile(args.midi_file)
+song.make_ticks_abs()
+tracks = []
+for track in song:
+    notes = [note for note in track if note.name == 'Note On']
+    pitch = [note.pitch for note in notes]
+    tick = [note.tick for note in notes]
+    tracks += [tick, pitch]
+plt.plot(*tracks)
+plt.show()
